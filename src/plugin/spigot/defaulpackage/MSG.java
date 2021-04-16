7f234f413b448a9f1043cfe3732e896d12ed180a
rutiglianoalberto97@gmail.com <rutiglianoalberto97@gmail.com>
2021-04-16 19:53:32 +0200

    Added other messages

--- src/plugin/spigot/defaulpackage/MSG.java
+++ src/plugin/spigot/defaulpackage/MSG.java
@@ -5,18 +5,17 @@ import java.util.Random;
 
 import org.bukkit.entity.Player;
 
-import net.md_5.bungee.api.ChatColor;
 
 import static net.md_5.bungee.api.ChatColor.*;
 
 public enum MSG {
 	
-	PLAYER_JOIN(new String[] {"[p] ha acceduto", "[p] è entrato", "Ecco [p] u trmon"}),
+	PLAYER_JOIN(new String[] {"[p] ha acceduto", "[p] è entrato", "Ecco [p] u trmon", "Un caldo benvenuto a [p] u trmon"}),
 	PLAYER_LEFT(new String[] {"[p] è scappato via", "[p] è uscito", "[p] è andato"}),
 	SLEEP(new String[] {"[p] sta dormendo", "[p] sà mìs a dorm", "[p] è andato a dormire", "[p] è andato a letto", "[p] dorme", "[p] sta già russando"}),
 	GET_DAMAGE(new String[] {"[p] ti ha inchiappettato", "[p] ti ha dato una botta", "[p] ti sta menando", "[p] ti ha fatto la bua", "[p] ha voglia di te"}),
-	WAKE_UP(new String[] {"[p] bastardo si è alzato", "[p] si è svegliato"}),
-	GOOD_MORNING(new String[] {"Buongiorno trimone", "Buongiorno, ben svegliato", "Buongiorno un cazzo"}),
+	WAKE_UP(new String[] {"[p] bastardo si è alzato", "[p] si è svegliato di soprassalto"}),
+	GOOD_MORNING(new String[] {"Buongiorno trimone", "Buongiorno cazzone", "Buongiornissimo, caffè?", "Buongiorno, ben svegliato", "Buongiorno un cazzo"}),
 	ENCHANTMENT(new String[] {"Il bastardo [p] ha incantato un oggetto", "[p] è un bastardo perchè ha incantato un oggetto", "Complimenti al bastardo [p] per aver incantato un oggetto"}),
 	IN_NETHER(new String[] {"[p] è entrato nel " + DARK_RED + "Nether"}),
 	IN_OVERWORLD(new String[] {"[p] è tornato nell'" + DARK_GREEN + "OverWorld"});
